68b69cb7a9eaa618f29b193f82c3cd52c662fdcf
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-11 22:52:15 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+
+*/
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let count = 0;
+    let h = [...heights];
+    let k = heights.sort((a,b)=> a-b)
+     
+     for(let i = 0; i < h.length; i++){
+          if(k[i] != h[i]) count++;
+     }
+
+    return count;
+};
